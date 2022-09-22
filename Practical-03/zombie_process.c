09109bbd0b70a0b47eb438485f0dc1124186d148
knockcat <vishujoshi21nov2000@gmail.com>
2022-09-23 01:14:15 +0530

    zombie process

---
+++ Practical-03/zombie_process.c
@@ -0,0 +1,31 @@
+/*
+    Write a C program to demonstrate Zombie Process.
+
+    Zombie Process is a process whose execution is finished but it still has an entry in the process table.
+    In this, the parent process is put to sleep for a long time while the child process is terminated using exit(0). When a child process is terminated using exit(0) then the parent process still reads the status of the child process.
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Name - Vishal Joshi\nSection - A\nStudent id - 20011368\n\n");
+
+    int pid = fork();
+
+    if (pid > 0)
+    {
+        sleep(10);
+        printf("\nThis is Parent Proces \n");
+        printf("PID : %d\n", getpid());
+    }
+    else
+    {
+        printf("\nThis is Child Process \n");
+        printf("PID : %d\n", getpid());
+        exit(0);
+    }
+    return 0;
+}
