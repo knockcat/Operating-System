6f6613679534e8cdfc97a298a61bc472c199830d
knockcat <vishujoshi21nov2000@gmail.com>
2022-12-28 13:37:44 +0530

    FCFS Disk Scheduling

---
+++ Practical-13/fcfs_disk_scheduling.c
@@ -0,0 +1,40 @@
+/*
+    Write a C program to implement FCFS disk scheduling algorithm in Operating Systems.
+
+    FCFS Scheduling Algorithm is the simplest Disk Scheduling algorithm. It services the IO requests in the order in which they arrive. There is no starvation in this algorithm, every request is serviced.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\nName - Vishal Joshi\nSection - A\nStudent id - 20011368\n\n");
+    int n, seektime = 0, head;
+
+    printf("Enter the number of sequence : ");
+    scanf("%d", &n);
+
+    int seq[n];
+
+    printf("Enter the Sequence : ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &seq[i]);
+    }
+
+    printf("Enter the position of head: ");
+    scanf("%d", &head);
+
+    printf("The FCFS Sequence is : ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", seq[i]);
+        seektime = seektime + abs(head - seq[i]);
+        head = seq[i];
+    }
+
+    printf("\nTotal Seek Time is : %d\n", seektime);
+
+    return 0;
+}
