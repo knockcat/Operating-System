bb33e7449eefb80049caf3bceb68801e8965baff
knockcat <vishujoshi21nov2000@gmail.com>
2022-12-28 14:19:46 +0530

    Zombie Process

--- Practical-03/zombie_process.c
+++ Practical-03/zombie_process.c
@@ -17,15 +17,17 @@ int main()
 
     if (pid > 0)
     {
-        printf("\nThis is Parent Process\n");
+        sleep(10);
+        printf("\nThis is Parent Proces \n");
         printf("PID : %d\n", getpid());
-        exit(0);
     }
     else
     {
-        sleep(10);
-        printf("\nThis is Child Process\n");
+        printf("\nThis is Child Process \n");
         printf("PID : %d\n", getpid());
+        printf("\nParent PID : %d\n", getppid());
+        exit(0);
     }
+
     return 0;
 }
